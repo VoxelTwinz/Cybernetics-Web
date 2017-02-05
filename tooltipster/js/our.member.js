8592d9f2b641b829d1913b0eb2fb4965a9e63779
MikiSahika <sableangle@gmail.com>
2017-02-05 14:02:34 +0800

    First on GitHub

---
+++ tooltipster/js/our.member.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+    $('#tooltip-MIKI').tooltipster({
+        content: $('<span><p><i class="fa fa-facebook-square "></i><a target="_blank" href="https://www.facebook.com/sableangle"> Facebook</a></p><p><i class="fa fa-envelope-o"></i><a href="mailto:sableangle@gmail.com"> sableangle@gmail.com</a></p></span>'),
+        theme: 'tooltipster-punk',
+        contentAsHTML: true,
+        position: 'bottom',
+		interactive: true
+    });
+    $('#tooltip-TUGO').tooltipster({
+        content: $('<span><p><i class="fa fa-facebook-square "></i><a target="_blank" href="https://www.facebook.com/go.tu.9"> Facebook</a></p><p><i class="fa fa-envelope-o"></i><a href="mailto:q4568522222@hotmail.com"> q4568522222@hotmail.com</a></p><p><i class="fa fa-home"></i><a target="_blank" href="http://home.gamer.com.tw/homeindex.php?owner=kintarou"> 巴哈小屋</a></p><p><i class="fa fa-home"></i><a target="_blank" href="http://www.pixiv.net/member.php?id=1797207"> PIXIV</a></p></span> '),
+        theme: 'tooltipster-punk',
+        contentAsHTML: true,
+        position: 'bottom',
+		interactive: true
+    });
+    $('#tooltip-JP').tooltipster({
+        content: $('<span><p><i class="fa fa-envelope-o"></i><a href="mailto:arrockansate@gmail.com"> arrockansate@gmail.com</a></p></span>'),
+        theme: 'tooltipster-punk',
+        contentAsHTML: true,
+        position: 'bottom',
+		interactive: true
+    });
+    $('#tooltip-SMT').tooltipster({
+        content: $('<span><p><i class="fa fa-facebook-square "></i><a target="_blank" href="https://www.facebook.com/shihmintin"> Facebook</a></p><p><i class="fa fa-envelope-o"></i><a href="mailto:kt3r5d641883@hotmail.com"> kt3r5d641883@livemail.tw</a></p></span>'),
+        theme: 'tooltipster-punk',
+        contentAsHTML: true,
+        position: 'bottom',
+		interactive: true
+    });
+    $('#tooltip-TYX').tooltipster({
+        content: $('<span><p><i class="fa fa-facebook-square "></i><a target="_blank" href="https://www.facebook.com/profile.php?id=100002007679177"> Facebook</a></p><p><i class="fa fa-envelope-o"></i><a href="mailto:tc94312@gmail.com"> tc94312@gmail.com</a></p></span>'),
+        theme: 'tooltipster-punk',
+        contentAsHTML: true,
+        position: 'bottom',
+		interactive: true
+    });
+});
